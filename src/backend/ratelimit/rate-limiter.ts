76a75e81caa3572f7fff3f2a7ad8e6f15efa044a
Patrick Bongalos <p.bongalos@toyota-mobilitysolutions.ph>
2026-03-04 15:40:38 +0800

    update code

--- src/backend/ratelimit/rate-limiter.ts
+++ src/backend/ratelimit/rate-limiter.ts
@@ -4,8 +4,6 @@ import { typedEnv } from "src/types/elysia";
 export function rateLimiter() {
   return new Elysia().use(typedEnv).onRequest(async ({ request, env }) => {
     const ip = request.headers.get("CF-Connecting-IP") || request.headers.get("x-forwarded-for") || "anonymous";
-    console.log("ip", ip);
-    console.log("working rate limiter");
     const { success } = await env.MY_RATE_LIMITER.limit({
       key: ip,
     });
